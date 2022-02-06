09480b9ed6dfe9c93906646112e675bdeb9bace9
r02092 <87572333+r02092@users.noreply.github.com>
2022-02-07 01:17:48 +0900

    phpinfo確認

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 	header('Access-Control-Allow-Origin: https://r02092.github.io');
 	$url=parse_url(getenv('DATABASE_URL'));
 	$dbh=new PDO('pgsql:dbname='.substr($url['path'],1).';host='.$url['host'],$url['user'],$url['pass']);
-	$stmt=$dbh->query('select * from information_schema.columns where table_name="rooms" order by ordinal_position;');
-	var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+	phpinfo();
 	/*
 	switch($_POST['act']){
 		case 'start':
