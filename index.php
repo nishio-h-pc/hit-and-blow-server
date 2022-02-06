e115c2944a72fef996fe805fe8655b086810fa0f
r02092 <87572333+r02092@users.noreply.github.com>
2022-02-07 02:29:12 +0900

    参照渡しに関するエラーを修正

--- index.php
+++ index.php
@@ -5,23 +5,25 @@
 	$dbh=new PDO('pgsql:dbname='.substr($url['path'],1).';host='.$url['host'],$url['user'],$url['pass']);
 	switch($_POST['act']){
 		case 'start':
+			$id=hexdec($_POST['id']);
 			$stmt=$dbh->prepare("UPDATE rooms SET num1='?',name1='?' WHERE id=?");
 			$stmt->setFetchMode(PDO::FETCH_ASSOC);
 			$stmt->bindParam(1,$_POST['num'],PDO::PARAM_STR);
 			$stmt->bindParam(2,$_POST['name'],PDO::PARAM_STR);
-			$stmt->bindParam(3,$_POST['id'],PDO::PARAM_INT);
+			$stmt->bindParam(3,$id,PDO::PARAM_INT);
 			$stmt->execute();
 			$stmt=$dbh->prepare('SELECT name0,duplicate FROM rooms WHERE id=?');
-			$stmt->bindParam(1,hexdec($_POST['id']),PDO::PARAM_INT);
+			$stmt->bindParam(1,$id,PDO::PARAM_INT);
 			$stmt->execute();
 			$res=$stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 			echo $res['duplicate'].$res['name0'];
 			break;
 		case 'waitStart':
+			$id=hexdec($_POST['id']);
 			do{
 				sleep(1);
 				$stmt=$dbh->prepare('SELECT name1 FROM rooms WHERE id=?');
-				$stmt->bindParam(1,hexdec($_POST['id']),PDO::PARAM_INT);
+				$stmt->bindParam(1,$id,PDO::PARAM_INT);
 				$stmt->execute();
 				$res=$stmt->fetchAll(PDO::FETCH_ASSOC)[0]['name1'];
 			}while(!is_null($res));
@@ -42,7 +44,7 @@
 				$stmt->bindParam(1,$id,PDO::PARAM_INT);
 				$stmt->execute();
 			}
-			$stmt=$dbh->prepare("INSERT INTO rooms (id,num0,name0,duplicate) VALUES (:id,:num0,:name0,:duplicate)");
+			$stmt=$dbh->prepare('INSERT INTO rooms (id,num0,name0,duplicate) VALUES (:id,:num0,:name0,:duplicate)');
 			$stmt->setFetchMode(PDO::FETCH_ASSOC);
 			$stmt->bindParam(':id0',$id,PDO::PARAM_INT);
 			$stmt->bindParam(':num0',$_POST['num'],PDO::PARAM_STR);
@@ -52,29 +54,31 @@
 			echo dechex($id);
 			break;
 		case 'wait':
+			$id=hexdec($_POST['id']);
 			$stmt=$dbh->prepare('SELECT time FROM rooms WHERE id=?');
-			$stmt->bindParam(1,hexdec($_POST['id']),PDO::PARAM_INT);
+			$stmt->bindParam(1,$id,PDO::PARAM_INT);
 			$stmt->execute();
 			$res=$stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 			do{
 				sleep(1);
 				$oldTime=$res['time'];
 				$stmt=$dbh->prepare('SELECT ans,time FROM rooms WHERE id=?');
-				$stmt->bindParam(1,hexdec($_POST['id']),PDO::PARAM_INT);
+				$stmt->bindParam(1,$id,PDO::PARAM_INT);
 				$stmt->execute();
 				$res=$stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 			}while($oldTime==$res['time']);
 			echo $res['ans'];
 			break;
 		case 'judge':
+			$id=hexdec($_POST['id']);
 			$stmt=$dbh->prepare("UPDATE rooms SET ans='?' WHERE id=?");
 			$stmt->bindParam(1,$_POST['ans'],PDO::PARAM_INT);
-			$stmt->bindParam(2,hexdec($_POST['id']),PDO::PARAM_INT);
+			$stmt->bindParam(2,$id,PDO::PARAM_INT);
 			$stmt->execute();
 			$res=$stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 			$stmt=$dbh->prepare('SELECT num? FROM rooms WHERE id=?');
 			$stmt->bindParam(1,$_POST['playerId'],PDO::PARAM_INT);
-			$stmt->bindParam(2,hexdec($_POST['id']),PDO::PARAM_INT);
+			$stmt->bindParam(2,$id,PDO::PARAM_INT);
 			$stmt->execute();
 			$hit=0;
 			$blow=0;
